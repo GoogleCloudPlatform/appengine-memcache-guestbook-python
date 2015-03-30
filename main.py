f273b08646bf87cf23fc7246a6fb8ef57a3149d0
Jeff Mendoza <jeffmendoza@google.com>
2015-03-30 13:37:40 -0700

    Add tags for docs.

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # Copyright 2015 Google Inc. All rights reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# [START all]
 import cgi
 import cStringIO
 import logging
@@ -60,6 +61,7 @@ class MainPage(webapp2.RequestHandler):
       </html>""".format(urllib.urlencode({'guestbook_name': guestbook_name}),
                         cgi.escape(guestbook_name)))
 
+    # [START check_memcache]
     def get_greetings(self, guestbook_name):
         """
         get_greetings()
@@ -79,7 +81,9 @@ class MainPage(webapp2.RequestHandler):
                                 greetings, 10):
                 logging.error('Memcache set failed.')
         return greetings
+    # [END check_memcache]
 
+    # [START query_datastore]
     def render_greetings(self, guestbook_name):
         """
         render_greetings()
@@ -106,6 +110,7 @@ class MainPage(webapp2.RequestHandler):
             output.write('<blockquote>{}</blockquote>'.format(
                              cgi.escape(greeting.content)))
         return output.getvalue()
+    # [END query_datastore]
 
 
 class Guestbook(webapp2.RequestHandler):
@@ -130,3 +135,4 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                ('/sign', Guestbook)],
                               debug=True)
 
+# [END all]
